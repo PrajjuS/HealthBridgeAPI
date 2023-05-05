ac79b8bd973834d5d7f35bf2a26885e416932355
PrajjuS <theprajjus@gmail.com>
2023-05-05 08:30:02 +0530

    HealthAPI: db: Add init method to the classes
    - To get args info while using classes
    - Meanwhile at it, also rearrange some columns and fix typo
    
    Change-Id: I2c790a49cad9415081a1ba9b8928b357e2306974
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- HealthAPI/db/hospital.py
+++ HealthAPI/db/hospital.py
@@ -5,11 +5,25 @@ from HealthAPI.db import BASE
 
 class Hospital(BASE):
     __table__ = "hospital"
-    hostp_id = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
+    hosp_id = Column(String, primary_key=True)
     password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     license = Column(String, nullable=False)
     address = Column(UnicodeText, nullable=False)
+
+    def __init__(
+        self,
+        hosp_id: str,
+        password: str,
+        name: str,
+        license: str,
+        address: str,
+    ):
+        self.hostp_id = hosp_id
+        self.password = password
+        self.name = name
+        self.license = license
+        self.address = address
 
     def __repr__(self):
         return f"Hospital('{self.hostp_id}', '{self.name}')"
--- HealthAPI/db/user.py
+++ HealthAPI/db/user.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import date, datetime
 
 from sqlalchemy import (
     BigInteger,
@@ -20,9 +21,25 @@ class User(BASE):
     password = Column(String, nullable=False)
     name = Column(String, nullable=False)
     dob = Column(Date, nullable=False)
-    address = Column(String, nullable=False)
     mobile = Column(BigInteger, nullable=False)
+    address = Column(String, nullable=False)
     reports = relationship("UserHealth", backref="hreport", lazy=True)
+
+    def __init__(
+        self,
+        user_id: str,
+        password: str,
+        name: str,
+        dob: date,
+        mobile: str,
+        address: str,
+    ):
+        self.user_id = user_id
+        self.password = password
+        self.name = name
+        self.dob = dob
+        self.mobile = mobile
+        self.address = address
 
     def __repr__(self):
         return f"User('{self.user_id}', '{self.name}')"
@@ -33,7 +50,12 @@ class UserHealth(BASE):
     id = Column(primary_key=True)
     date_time = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     report = Column(UnicodeText, nullable=False)
-    user = Column(String, ForeignKey("user.user_id"), nullable=False)
+    user_id = Column(String, ForeignKey("user.user_id"), nullable=False)
+
+    def __init__(self, date_time: datetime, report: str, user_id: str):
+        self.date_time = date_time
+        self.report = report
+        self.user_id = user_id
 
     def __repr__(self):
         return f"UserHealth('{self.date_time}', '{self.id}')"
